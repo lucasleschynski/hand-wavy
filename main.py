49f64a79164bb2f8b50be6d20553ba2d8d455de7
Lucas Leschynski <lucas.leschynski@gmail.com>
2023-04-20 21:18:46 +0100

    Added comments to main loop

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ def main():
     capture = cv2.VideoCapture(0)
     extractor = HandExtractor(capture)
 
+    # Main loop (video feed -> gesture detection -> drawing)
     while True:
+        # updates gesture coordinates and landmarked frame (optional)
         extractor.process_frame()
         landmarked_frame = extractor.get_landmarked_frame()
         cv2.imshow("Image Feed", landmarked_frame)
 
+        # get index/thumb tip coords
         pinch_coord = extractor.get_gesture_coords()
+
+        if pinch_coord:
+            # Do calcuations
+            continue
 
         if cv2.waitKey(1) == 'q':
             cv2.destroyAllWindows()
